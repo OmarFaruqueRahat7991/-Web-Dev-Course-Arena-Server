53a3e478d9027e50cc5c2415c4ad447c89dabe72
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-10-25 19:22:37 +0600

    second commit

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 
-
+const categories = require('./Data/Categories.json');
 
 app.get('/', (req, res) => {
   res.send('Course API Running.')
 })
 
+app.get('/categories', (req, res) => {
+  res.send(categories);
+})
 
 app.listen(port, () => {
   console.log(`Course API Running ${port}`)
