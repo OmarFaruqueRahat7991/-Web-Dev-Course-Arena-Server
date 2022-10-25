f8931f3696809b8ac2bf7538e5435882a36bc24e
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-10-25 20:12:01 +0600

    fourth commit

--- index.js
+++ index.js
@@ -16,8 +16,10 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
   res.send(categories);
 })
-app.get('/courses', (req, res) => {
-  res.send(courses);
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const particularCourse = courses.find(course => course.id === id);
+    res.send(particularCourse);
 })
 
 app.listen(port, () => {
