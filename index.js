87c173a78a00005b9ba90345d467c2d09f9c085c
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-10-28 03:51:28 +0600

    another commit

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require("./Data/Categories.json");
+const courses = require("./Data/Courses.json");
 
-const categories = require('./Data/Categories.json');
-const courses = require('./Data/Courses.json');
+app.get("/", (req, res) => {
+  res.send("Course API Running.");
+});
 
-app.get('/', (req, res) => {
-  res.send('Course API Running.');
-})
-
-app.get('/categories', (req, res) => {
+app.get("/categories", (req, res) => {
   res.send(categories);
-})
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    if(id === "21"){
+});
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if(id === "08"){
       res.send(courses);
     }
-    else{
-      const categoryNews = courses.filter(course => course.id === id);
-      res.send(categoryNews);
-    }
-})
-app.get('/allcourses', (req, res) => {
+    else{}
+  const categoryNews = courses.filter((course) => course.category_id === id);
+  res.send(categoryNews);
+});
+
+app.get("/allcourses", (req, res) => {
   res.send(courses);
-})
-app.get('/course/:id', (req, res) => {
-    const id = req.params.id;
-    const particularCourse = courses.find(course => course.id === id);
-    res.send(particularCourse);
-})
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course._id === id);
+  res.send(selectedCourse);
+});
 
 app.listen(port, () => {
   console.log(`Course API Running ${port}`);
-})
+});
